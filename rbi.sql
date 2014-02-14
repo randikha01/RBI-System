0792e6e00f820310d67c508fb34c5851c70997ce
andigaluh <andi@komunigrafik.com>
2014-02-14 11:10:59 +0700

    update rbi.sql

--- rbi.sql
+++ rbi.sql
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `rbi`
 --
-CREATE DATABASE IF NOT EXISTS `rbi` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `rbi`;
 
 -- --------------------------------------------------------
 
